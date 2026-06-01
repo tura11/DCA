a96567a3c9a8fa7d9d72ce5c4f3a840e72682c73
Tymek <tymek_uramek@wp.pl>
2026-06-01 22:27:12 +0200

    TokenX for DCA logic

---
+++ src/TokenX.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract TokenX is ERC20{
+
+
+    constructor()ERC20("TokenX", "TKX"){
+        _mint(msg.sender, 1000e18);
+    }
+
+}
