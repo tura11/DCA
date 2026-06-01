bfb38fee6b9954af76646d3f246b7980f47b882c
Tymek <tymek_uramek@wp.pl>
2026-06-01 22:32:11 +0200

    DCA struct

---
+++ src/DCA.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+import {TokenX} from "./src/TokenX.sol";
+
+
+contract DCA {
+
+    struct Position{
+        uint256 amount;
+        uint256 period;
+        uint256 lastExecuted;
+        bool active;
+    }
+
+    function setPosition()
+
+
+
+
+}
