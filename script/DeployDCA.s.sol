0dbad29fd035a5471d5f7109f74d47c628a83562
Tymek <tymek_uramek@wp.pl>
2026-06-08 19:13:37 +0200

    Script for deploy

---
+++ script/DeployDCA.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {DCA} from "../src/DCA.sol";
+
+contract DeployDCA is Script {
+    function run() external returns (DCA) {
+        vm.startBroadcast();
+        DCA dca = new DCA();
+        vm.stopBroadcast();
+        return dca;
+    }
+}
