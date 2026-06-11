b18b689d9d8d1e14eabe14889a135de3d9b0d5c0
Tymek <tymek_uramek@wp.pl>
2026-06-11 19:59:36 +0200

    100% coverage

--- test/DCA.t.sol
+++ test/DCA.t.sol
@@ -1,29 +1,25 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.30;
-
-
 
 import {DCA} from "../src/DCA.sol";
 import {Test} from "forge-std/Test.sol";
 
-
-
-contract DCATest is Test{
+contract DCATest is Test {
     DCA public dca;
     address user;
+    address user2;
 
     function setUp() public {
         dca = new DCA();
         user = makeAddr("user");
+        user2 = makeAddr("user2");
         vm.deal(user, 10 ether);
+        vm.deal(user2, 10 ether);
     }
-
 
     function testConstructor() public view {
         assertEq(dca.tokenBalanceOf(address(dca)), 1000000e18);
     }
-
 
     function testDeposit() external {
         vm.prank(user);
@@ -31,13 +27,19 @@ contract DCATest is Test{
         assertEq(dca.balanceOf(user), 5 ether);
     }
 
-    function testDepositRevert() external{
+    function testDepositRevert() external {
         vm.prank(user);
         vm.expectRevert(DCA.DCA__AmountCantBeZero.selector);
         dca.deposit{value: 0}();
-        
     }
 
+
+    function testReceiveFunctionDirectDeposit() external {
+        vm.prank(user);
+        (bool success, ) = address(dca).call{value: 2 ether}("");
+        assertTrue(success);
+        assertEq(dca.balanceOf(user), 2 ether);
+    }
 
     function testSetPosition() external {
         vm.startPrank(user);
@@ -45,6 +47,22 @@ contract DCATest is Test{
         dca.setPosition(1000, 3 days);
         assertEq(dca.getUserPositionAmount(user), 1000);
         assertEq(dca.getUserPositionPeriod(user), 3 days);
+        vm.stopPrank();
+    }
+
+   
+    function testSetPositionDoesNotDuplicateUser() external {
+        vm.startPrank(user);
+        dca.deposit{value: 5 ether}();
+        dca.setPosition(1000, 3 days);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        dca.cancel();
+        dca.setPosition(2000, 4 days);
+        vm.stopPrank();
+
+        assertEq(dca.users(0), user);
     }
 
     function testSetPositionRevertAmountZero() public {
@@ -52,6 +70,7 @@ contract DCATest is Test{
         dca.deposit{value: 5 ether}();
         vm.expectRevert(DCA.DCA__AmountCantBeZero.selector);
         dca.setPosition(0, 3 days);
+        vm.stopPrank();
     }
 
     function testSetPostionRevertPeriodMustBeMoreThanMinute() public {
@@ -59,8 +78,8 @@ contract DCATest is Test{
         dca.deposit{value: 5 ether}();
         vm.expectRevert(DCA.DCA__PeriodMustBeMoreThanMinute.selector);
         dca.setPosition(1000, 10);
+        vm.stopPrank();
     }
-
 
     function testSetPositionRevertPositionAlreadyActive() public {
         vm.startPrank(user);
@@ -68,42 +87,128 @@ contract DCATest is Test{
         dca.setPosition(1000, 3 days);
         vm.expectRevert(DCA.DCA__PositionAlreadyActive.selector);
         dca.setPosition(1000, 7 days);
+        vm.stopPrank();
     }
-
 
     function testSuccesfulWithdrawal() public {
         vm.startPrank(user);
         dca.deposit{value: 5 ether}();
         dca.setPosition(1000, 3 days);
-        vm.warp(4 days);
+        vm.warp(block.timestamp + 4 days);
         dca.withdraw();
         vm.stopPrank();
 
         assertEq(dca.tokenBalanceOf(user), 1000);
-
-
     }
 
+    function testWithdrawRevertPositonNotActive() public {
+        vm.startPrank(user);
+        dca.deposit{value: 5 ether}();
+        vm.expectRevert(DCA.DCA__PositionNotActive.selector);
+        dca.withdraw();
+        vm.stopPrank();
+    }
+
+    function testWithdrawRevertYouCannotWithdrawYet() public {
+        vm.startPrank(user);
+        dca.deposit{value: 5 ether}();
+        dca.setPosition(1000, 3 days);
+        vm.warp(block.timestamp + 2 days);
+        vm.expectRevert(DCA.DCA__YouCannotWithdrawYet.selector);
+        dca.withdraw();
+        vm.stopPrank();
+    }
+
+    function testWithdrawRevertNotEnoughMoney() public {
+        vm.startPrank(user);
+        dca.deposit{value: 1 ether}();
+        dca.setPosition(2000, 3 days);
+        vm.warp(block.timestamp + 4 days);
+        vm.expectRevert(DCA.DCA__NotEnoughMoneyForWithdrawal.selector);
+        dca.withdraw();
+        vm.stopPrank();
+    }
+
+
+    function testWithdrawDeactivatesPositionWhenBalanceGoesToZero() public {
+        vm.startPrank(user);
+      
+        dca.deposit{value: 1 ether}();
+        dca.setPosition(1000, 3 days);
+        vm.warp(block.timestamp + 4 days);
+        
+        dca.withdraw();
+        
+        assertEq(dca.balanceOf(user), 0);
+        
+  
+        DCA.Position memory position = dca.getUserPostions(user);
+        assertFalse(position.active);
+        
+        vm.stopPrank();
+    }
+
+  
+    
+ 
+
+    function testUpdatePositionAmountSuccessful() public {
+        vm.startPrank(user);
+        dca.deposit{value: 5 ether}();
+        dca.setPosition(1000, 3 days);
+        
+ 
+        dca.updatePositionAmount(2500);
+        
+        assertEq(dca.getUserPositionAmount(user), 2500);
+        assertEq(dca.getUserPositionPeriod(user), 3 days);
+        vm.stopPrank();
+    }
+
+    function testUpdatePositionAmountRevertPositionNotActive() public {
+        vm.startPrank(user);
+     
+        vm.expectRevert(DCA.DCA__PositionNotActive.selector);
+        dca.updatePositionAmount(1500);
+        vm.stopPrank();
+    }
+
+    function testUpdatePositionAmountRevertAmountCantBeZero() public {
+        vm.startPrank(user);
+        dca.deposit{value: 5 ether}();
+        dca.setPosition(1000, 3 days);
+      
+        vm.expectRevert(DCA.DCA__AmountCantBeZero.selector);
+        dca.updatePositionAmount(0);
+        vm.stopPrank();
+    }
+
+    // -------------------------------------------------------------------------------
 
     function testCancelSuccesful() public {
         vm.startPrank(user);
         dca.deposit{value: 5 ether}();
         dca.setPosition(1000, 3 days);
-        vm.warp(4 days);
+        vm.warp(block.timestamp + 4 days);
         dca.withdraw();
         assertEq(dca.tokenBalanceOf(user), 1000);
         dca.cancel();
         assertEq(dca.balanceOf(user), 4 ether);
-        
+        vm.stopPrank();
+    }
+
+    function testCancelRevertPositionNotActive() public {
+        vm.startPrank(user);
+        vm.expectRevert(DCA.DCA__PositionNotActive.selector);
+        dca.cancel();
+        vm.stopPrank();
     }
 
     function testCheckUpkeepReturnsFalseWhenNoPositions() public {
         (bool upkeepNeeded, bytes memory performData) = dca.checkUpkeep("");
-
         assertFalse(upkeepNeeded);
         assertEq(performData.length, 0);
     }
-
 
     function testCheckUpKeepRetrunsFalseWhenPeriodisNotPassed() public {
         vm.startPrank(user);
@@ -113,7 +218,7 @@ contract DCATest is Test{
 
         vm.warp(block.timestamp + 1 days);
 
-        (bool upkeepNeeded,) = dca.checkUpkeep("");
+        (bool upkeepNeeded, ) = dca.checkUpkeep("");
         assertFalse(upkeepNeeded);
     }
 
@@ -125,26 +230,21 @@ contract DCATest is Test{
 
         vm.warp(block.timestamp + 4 days);
 
-
         (bool upkeepNeeded, bytes memory performData) = dca.checkUpkeep("");
         assertTrue(upkeepNeeded);
         address decodedUser = abi.decode(performData, (address));
         assertEq(decodedUser, user);
     }
 
-
     function testPerformUpkeepWithdrawsTokens() public {
         vm.startPrank(user);
-
         dca.deposit{value: 5 ether}();
         dca.setPosition(1000, 3 days);
-
         vm.stopPrank();
 
         vm.warp(block.timestamp + 4 days);
 
         (, bytes memory performData) = dca.checkUpkeep("");
-
         dca.performUpkeep(performData);
 
         assertEq(dca.tokenBalanceOf(user), 1000);
